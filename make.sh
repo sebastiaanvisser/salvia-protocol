f358ecb4e21495f0541cb77bf4789486e44f7656
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-09-02 11:56:37 +0100

    From salvia to salvia-protocol.

--- make.sh
+++
@@ -1,6 +0,0 @@
-clear
-cabal configure &&
-cabal build &&
-cabal install &&
-./dist/build/salvia-demo/salvia-demo +RTS -N2
-
--- src/Network/Salvia.hs
+++
@@ -1,8 +0,0 @@
-module Network.Salvia              {- doc ok -}
-  ( module Network.Salvia.Httpd
-  , module Network.Salvia.Handlers
-  )
-where
-
-import Network.Salvia.Httpd
-import Network.Salvia.Handlers
